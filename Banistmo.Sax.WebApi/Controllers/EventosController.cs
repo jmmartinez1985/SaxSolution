b40631379b05915e9c8674a59d62d1412ebbeafd
jmcclean3 <jmcclean3@hotmail.com>
2018-03-07 00:04:12 -0500

    Nueva clase Eventos en WebApi

---
+++ Banistmo.Sax.WebApi/Controllers/EventosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Banistmo.Sax.Services.Interfaces.Business;
+using Banistmo.Sax.Services.Models;
+namespace Banistmo.Sax.WebApi.Controllers
+{
+    [RoutePrefix("api/Eventos")]
+    public class EventosController : ApiController
+    {
+        private readonly IEventosService eventoService;
+
+        public EventosController()
+        {
+        }
+
+        public EventosController(IEventosService ev)
+        {
+            eventoService = ev;
+        }
+
+        public IHttpActionResult Get()
+        {
+            List<EventosModel> eve = eventoService.GetAll();
+            if (eve == null)
+            {
+                return NotFound();
+            }
+            return Ok(eve);
+        }
+    }
+}
