09b8c65e60cc4dd9ce5e878a3446b53524ee6d71
IsrraBanton <isrrael_01@yahoo.com>
2018-03-11 00:13:02 -0500

    PARAMETRO TEMPORAL

---
+++ Banistmo.Sax.Repository/Implementations/Business/ParametroTemp.cs
@@ -0,0 +1,35 @@
+﻿using Banistmo.Sax.Repository.Interfaces.Business;
+using Banistmo.Sax.Repository.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Banistmo.Sax.Repository.Interfaces;
+using Banistmo.Sax.Common;
+
+namespace Banistmo.Sax.Repository.Implementations.Business
+{
+    [Injectable]
+    public class ParametroTemp : RepositoryBase<SAX_PARAMETRO_TEMP>, IParametroTemp
+    {
+        public ParametroTemp()
+            : this(new SaxRepositoryContext())
+        {
+        }
+        public ParametroTemp(IRepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+        public override Expression<Func<SAX_PARAMETRO_TEMP, bool>> GetFilters()
+        {
+            throw new NotImplementedException();
+        }
+        public override Expression<Func<SAX_PARAMETRO_TEMP, bool>> SearchFilters(SAX_PARAMETRO_TEMP obj)
+        {
+            return x => x.PA_ID_PARAMETRO_TEMP == obj.PA_ID_PARAMETRO_TEMP;
+        }
+
+    }
+}
---
+++ Banistmo.Sax.Services/Implementations/Business/ParametroTempService.cs
@@ -0,0 +1,27 @@
+﻿using Banistmo.Sax.Repository.Implementations.Business;
+using Banistmo.Sax.Repository.Model;
+using Banistmo.Sax.Services.Interfaces.Business;
+using Banistmo.Sax.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banistmo.Sax.Repository.Implementations;
+using Banistmo.Sax.Common;
+
+namespace Banistmo.Sax.Services.Implementations.Business
+{
+    [Injectable]
+    public class ParametroTempService : ServiceBase<ParametroTempModel, SAX_PARAMETRO_TEMP, ParametroTemp>, IParametroTempService
+    {
+        public ParametroTempService()
+            : this(new ParametroTemp())
+        {
+
+        }
+        public ParametroTempService(ParametroTemp objParamTemp)
+            : base(objParamTemp)
+        { }
+    }
+}
