7c0f8b3f00cda8a6e56a5b60adb7dd2bec967e99
stn_r <stn_r@DESKTOP-T36SFN9>
2018-03-13 13:36:31 -0500

    Model Issue

--- Banistmo.Sax.Services/Models/EmpresaCentroModel.cs
+++ Banistmo.Sax.Services/Models/EmpresaCentroModel.cs
@@ -8,5 +8,13 @@ namespace Banistmo.Sax.Services.Models
 {
     public class EmpresaCentroModel
     {
+        public int EC_ID_REGISTRO { get; set; }
+        public int CE_ID_EMPRESA { get; set; }
+        public int CC_ID_CENTRO_COSTO { get; set; }
+        public int EC_ESTATUS { get; set; }
+        public System.DateTime EC_FECHA_CREACION { get; set; }
+        public string EC_USUARIO_CREACION { get; set; }
+        public Nullable<System.DateTime> EC_FECHA_MOD { get; set; }
+        public string EC_USUARIO_MOD { get; set; }
     }
 }
