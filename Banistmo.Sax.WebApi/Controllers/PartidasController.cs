0ee26a6d1f055d397f3df8c51792528cb5d0128d
stn_r <stn_r@DESKTOP-T36SFN9>
2018-03-14 21:21:02 -0500

    Partidas Pagination Sample

---
+++ Banistmo.Sax.WebApi/Controllers/PartidasController.cs
@@ -0,0 +1,60 @@
+﻿using Banistmo.Sax.Services.Interfaces.Business;
+using Banistmo.Sax.Services.Models;
+using Banistmo.Sax.WebApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Banistmo.Sax.WebApi.Controllers
+{
+    [RoutePrefix("api/Partidas")]
+    public class PartidasController : ApiController
+    {
+        private readonly IPartidasService partidasService;
+
+        public PartidasController(IPartidasService part) {
+            partidasService = part;
+        }
+
+        public IHttpActionResult Get()
+        {
+            List<PartidasModel> mdl = partidasService.GetAll();
+            if (mdl == null)
+            {
+                return NotFound();
+            }
+            return Ok(mdl);
+        }
+
+        [Route("GetPartidaPag")]
+        public IHttpActionResult GetPagination([FromUri]PagingParameterModel pagingparametermodel)
+        {
+            var source = partidasService.GetAll().OrderBy(c => c.PA_REGISTRO);
+            int count = source.Count();
+            int CurrentPage = pagingparametermodel.pageNumber;
+            int PageSize = pagingparametermodel.pageSize;
+            int TotalCount = count;
+            int TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+            var items = source.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
+            var previousPage = CurrentPage > 1 ? "Yes" : "No";
+            var nextPage = CurrentPage < TotalPages ? "Yes" : "No";
+            var paginationMetadata = new
+            {
+                totalCount = TotalCount,
+                pageSize = PageSize,
+                currentPage = CurrentPage,
+                totalPages = TotalPages,
+                previousPage,
+                nextPage
+            };
+            HttpContext.Current.Response.Headers.Add("Paging-Headers", JsonConvert.SerializeObject(paginationMetadata));
+            return Ok(items);
+
+        }
+    }
+}
