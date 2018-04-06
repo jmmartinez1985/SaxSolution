bee7383c802fa9467aadd128989f0237ba531b15
VictorMurillo <VictorMurillo@DESKTOP-OIGED8S>
2018-04-06 09:47:12 -0500

    Ajustes en el api de registroControl

---
+++ Banistmo.Sax.Repository/Interfaces/Business/IOnlyRegistroControl.cs
@@ -0,0 +1,13 @@
+﻿using Banistmo.Sax.Repository.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banistmo.Sax.Repository.Interfaces.Business
+{
+    public interface IOnlyRegistroControl: IRepository<SAX_REGISTRO_CONTROL>
+    {
+    }
+}
