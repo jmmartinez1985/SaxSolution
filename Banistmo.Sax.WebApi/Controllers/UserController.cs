fc68f3ebabdbe05f06b64b9a6920b74c3a0ea086
jmcclean3 <jmcclean3@hotmail.com>
2018-04-09 19:18:04 -0500

    actualizaciones con nombres mas correctos par Anthony

--- Banistmo.Sax.WebApi/Controllers/UserController.cs
+++ Banistmo.Sax.WebApi/Controllers/UserController.cs
@@ -315,7 +315,7 @@ namespace Banistmo.Sax.WebApi.Controllers
 
        // [Route("UserValidation"), HttpPut]
         [Route("UserToValidate"), HttpGet]
-        public IHttpActionResult validationUser(String UserToValidate)
+        public IHttpActionResult validationUser(string UserToValidate)
         {
             //CODIGO PARA OBTENER EL USUARIO Y CONTRASEÑA DEL DIRECTORIO ACTIVO DESDE EL WEBCONFIG
             //var a = directorioactivo.validaUsuarioLDAP(Properties.Settings.Default.userServiceDA, Properties.Settings.Default.passwordServiceDA, Properties.Settings.Default.loginIntranet,Properties.Settings.Default.dominioDa, userPar.UserToValidate);
