f552a7df5a8a428c2e761d3096b23dbaeda32dcb
jmcclean3 <jmcclean3@hotmail.com>
2018-04-23 22:13:11 -0500

    eventos ListarEventos

--- Banistmo.Sax.WebApi/Controllers/EventosController.cs
+++ Banistmo.Sax.WebApi/Controllers/EventosController.cs
@@ -43,8 +43,8 @@ namespace Banistmo.Sax.WebApi.Controllers
             }
         }
 
-        [Route("GetAll"), HttpGet]
-        public IHttpActionResult GetAll()
+        [Route("ListarEventos"), HttpGet]
+        public IHttpActionResult ListarEventos()
         {
             var evnt = eventoService.GetAll();
                        
@@ -53,8 +53,32 @@ namespace Banistmo.Sax.WebApi.Controllers
                 return BadRequest("No se puedo listar los eventos.");
             }
             else
-            {              
-                return Ok(evnt);
+            {
+                var eve = evnt.Select(ev => new
+                {
+                     EV_COD_EVENTO = ev.EV_COD_EVENTO
+                    ,CE_ID_EMPRESA = ev.CE_ID_EMPRESA
+                    ,NOMBRE_EMPRESA = ev.SAX_EMPRESA.CE_NOMBRE
+                    ,EV_ID_AREA = ev.EV_ID_AREA
+                    ,EV_DESCRIPCION_EVENTO = ev.EV_DESCRIPCION_EVENTO
+                    ,EV_CUENTA_DEBITO = ev.EV_CUENTA_DEBITO
+                    ,NOMBRE_CTA_DEBITO = ev.SAX_CUENTA_CONTABLE.CO_NOM_CUENTA
+                    ,EV_CUENTA_CREDITO = ev.EV_CUENTA_CREDITO
+                    ,NOMBRE_CTA_CREDITO = ev.SAX_CUENTA_CONTABLE1.CO_NOM_CUENTA
+                    ,EV_REFERENCIA = ev.EV_REFERENCIA
+                    ,EV_ESTATUS_ACCION = ev.EV_ESTATUS_ACCION
+                    ,EV_ESTATUS = ev.EV_ESTATUS
+                    ,EV_FECHA_CREACION = ev.EV_FECHA_CREACION
+                    ,EV_USUARIO_CREACION = ev.EV_USUARIO_CREACION
+                    ,NOMBRE_USER_CREA = ev.AspNetUsers.FirstName
+                    ,EV_FECHA_MOD = ev.EV_FECHA_MOD
+                    ,EV_USUARIO_MOD = ev.EV_USUARIO_MOD
+                    ,NOMBRE_USER_MOD = ev.AspNetUsers1.FirstName
+                    ,EV_FECHA_APROBACION = ev.EV_FECHA_APROBACION
+                    ,EV_USUARIO_APROBADOR = ev.EV_USUARIO_APROBADOR
+                    ,NOMBRE_USER_APROB = ev.AspNetUsers2.FirstName
+                });
+                return Ok(eve);
             }                        
         }
 
