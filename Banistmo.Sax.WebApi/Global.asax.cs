dc720f5b507367e98c87e35705727780ec7133e2
VictorMurillo <VictorMurillo@DESKTOP-OIGED8S>
2018-04-25 19:05:57 -0500

    Access-Control-Allow-Origin

--- Banistmo.Sax.WebApi/Global.asax.cs
+++ Banistmo.Sax.WebApi/Global.asax.cs
@@ -3,6 +3,7 @@ using Banistmo.Sax.WebApi.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -35,6 +36,19 @@ namespace Banistmo.Sax.WebApi
         //        HttpContext.Current.Response.End();
         //    }
         //}
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.HttpMethod == "OPTIONS")
+            {
+                Response.StatusCode = (int)HttpStatusCode.OK;
+                Response.AppendHeader("Access-Control-Allow-Origin", Request.Headers.GetValues("Origin")[0]);
+                Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+                Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                Response.AppendHeader("Access-Control-Allow-Credentials", "true");
+                Response.End();
+            }
+        }
 
     }
 }
