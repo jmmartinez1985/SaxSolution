73e4a3d872eb63828270e4b87d675c355d628d2c
jmcclean3 <jmcclean3@hotmail.com>
2018-04-27 21:03:17 -0500

    actualización de Eventos

--- Banistmo.Sax.Services/Models/EventosTempModel.cs
+++ Banistmo.Sax.Services/Models/EventosTempModel.cs
@@ -14,8 +14,8 @@ namespace Banistmo.Sax.Services.Models
         public int CE_ID_EMPRESA { get; set; }
         public int EV_ID_AREA { get; set; }
         public string EV_DESCRIPCION_EVENTO { get; set; }
-        public int EV_CUENTA_DEBITO { get; set; }
-        public int EV_CUENTA_CREDITO { get; set; }
+        public Int32 EV_CUENTA_DEBITO { get; set; }
+        public Int32 EV_CUENTA_CREDITO { get; set; }
         public string EV_REFERENCIA { get; set; }
         public string EV_ESTATUS_ACCION { get; set; }
         public int? EV_ESTATUS { get; set; }
@@ -25,5 +25,13 @@ namespace Banistmo.Sax.Services.Models
         public string EV_USUARIO_MOD { get; set; }
         public DateTime? EV_FECHA_APROBACION { get; set; }
         public string EV_USUARIO_APROBADOR { get; set; }
+
+        public virtual AspNetUserModel AspNetUsers { get; set; }
+        public virtual AspNetUserModel AspNetUsers1 { get; set; }
+        public virtual AspNetUserModel AspNetUsers2 { get; set; }
+        public virtual CuentaContableModel SAX_CUENTA_CONTABLE { get; set; }
+        public virtual CuentaContableModel SAX_CUENTA_CONTABLE1 { get; set; }
+        public virtual EmpresaModel SAX_EMPRESA { get; set; }
+        public virtual EventosTempModel EventoTemporal { get; set; }
     }
 }
