b7f2ab2e0c3ee246ef0814736eeabd9909a6b3c8
jmcclean3 <jmcclean3@hotmail.com>
2018-05-04 21:53:10 -0500

    int insert evento

--- Banistmo.Sax.Repository/Interfaces/Business/IEventos.cs
+++ Banistmo.Sax.Repository/Interfaces/Business/IEventos.cs
@@ -9,7 +9,7 @@ namespace Banistmo.Sax.Repository.Interfaces.Business
 {
     public interface IEventos : IRepository<SAX_EVENTO>
     {
-        bool Insert_Eventos_EventosTempOperador(SAX_EVENTO eve);
+        int Insert_Eventos_EventosTempOperador(SAX_EVENTO eve);
 
         bool Update_EventoTempOperador(SAX_EVENTO_TEMP eventoTempNuevo);
 
@@ -17,7 +17,7 @@ namespace Banistmo.Sax.Repository.Interfaces.Business
 
         bool Deshacer_EventoTempOperador(int eventoid);
 
-        bool SupervidorAprueba_Evento(int eventoId,string userId);
+        int SupervidorAprueba_Evento(int eventoId,string userId);
 
         bool SupervidorRechaza_Evento(int eventoId);
         //List<SAX_EVENTO> SearchByFilter(Int32 IdEmp, Int32 IdAreaOpe, string IdCuentaDb, string IdCuentaCR);
