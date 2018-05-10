0a979c250d8f40962b69a9e7390897f5bd3d5a25
VictorMurillo <VictorMurillo@DESKTOP-OIGED8S>
2018-05-10 11:34:38 -0500

    Modelo partida manual

---
+++ Banistmo.Sax.Services/Models/PartidaManualModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banistmo.Sax.Services.Models
+{
+    public class PartidaManualModel: PartidasModel
+    {
+        public string PA_EVENTO { get; set; }
+        public string PA_CREDITO { get; set; }
+        public string PA_DEBITO { get; set; } 
+        public string PA_NOMBRE_C { get; set; }
+        public string PA_NOMBRE_D { get; set; }
+    }
+}
--- Banistmo.Sax.WebApi/Controllers/UsuarioEmpresaController.cs
+++ Banistmo.Sax.WebApi/Controllers/UsuarioEmpresaController.cs
@@ -69,7 +69,30 @@ namespace Banistmo.Sax.WebApi.Controllers
 
             return NotFound();
         }
+        [Route("GetEmpresasByUserLogin"),HttpGet]
+        public IHttpActionResult GetEmpresasByUserLogin() {
+            string idUser=User.Identity.GetUserId();
+            int activo = Convert.ToInt16(BusinessEnumerations.Estatus.ACTIVO);
+            var usuarioEmpresa = usuarioEmpresaService.GetAll(c => c.US_ID_USUARIO == idUser && c.UE_ESTATUS==activo, null, includes: c => c.SAX_EMPRESA);
+            if (usuarioEmpresa == null)
+            {
+                return NotFound(); 
+            }
+            List<EmpresaModel> listEmp = new List<EmpresaModel>();
 
+            foreach (var emp in usuarioEmpresa.ToList())
+            {
+                listEmp.Add(emp.SAX_EMPRESA);
+            }
+
+            return Ok(listEmp.Select(c => new
+            {
+                CE_ID_EMPRESA = c.CE_ID_EMPRESA,
+                CE_COD_EMPRESA = c.CE_COD_EMPRESA,
+                CE_NOMBRE = c.CE_COD_EMPRESA+"-"+c.CE_NOMBRE
+            }));
+
+        }
         [Route("GetEmpresasByUser")]
         public IHttpActionResult GetEmpresasByUser(string id)
         {
