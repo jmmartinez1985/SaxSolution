9bbf20522abd9148a8d706eee32cae2858c2be49
Yuly Pino <yuly.pino@banistmo.com>
2018-05-30 08:33:29 -0500

    Se cargan objetos para creacion de reporte de saldos contables

---
+++ Banistmo.Sax.WebApi/Models/ParametersSaldoContable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Banistmo.Sax.WebApi.Models
+{
+    public class ParametersSaldoContable
+    {
+        public int? IdEmpresa { get; set; }
+        public Nullable<System.DateTime> FechaCorte { get; set; }
+        public int? IdCuentaContable { get; set; }
+        public int? IdAreaOperativa { get; set; }
+
+    }
+}
