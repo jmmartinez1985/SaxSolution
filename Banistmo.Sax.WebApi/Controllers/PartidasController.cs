353fdbdc6778abd33d8097876c891f7b8c187453
jmcclean3 <jmcclean3@hotmail.com>
2018-06-11 21:04:43 -0500

    partidas

--- Banistmo.Sax.WebApi/Controllers/PartidasController.cs
+++ Banistmo.Sax.WebApi/Controllers/PartidasController.cs
@@ -209,6 +209,71 @@ namespace Banistmo.Sax.WebApi.Controllers
             }
         }
 
+        [Route("GetPartidaPorOperacion"), HttpGet]
+        public async Task<IHttpActionResult> GetPartidaPorOperacion([FromUri] ParameterReportePartidaModel pagingparametermodel)
+        {
+            try
+            {
+                int capManual = Convert.ToInt16(BusinessEnumerations.TipoOperacion.CAPTURA_MANUAL);
+                int capInicial = Convert.ToInt16(BusinessEnumerations.TipoOperacion.CARGA_INICIAL);
+                int aprobado = Convert.ToInt16(BusinessEnumerations.EstatusCarga.APROBADO);
+                int anulado = Convert.ToInt16(BusinessEnumerations.EstatusCarga.ANULADO);
+                //int concilia = Convert.ToInt16(BusinessEnumerations.EstatusCarga.POR_CONCILIAR);
+
+                IdentityUser user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                var userArea = usuarioAreaService.GetSingle(d => d.US_ID_USUARIO == user.Id);
+
+                var model = partidasAprobadas.GetAllFlatten<PartidasAprobadasModel>(
+
+                    p => p.PA_STATUS_PARTIDA == aprobado
+                                            || p.PA_STATUS_PARTIDA == anulado
+                                             && p.PA_ESTADO_CONCILIA == 0
+                                             && p.PA_REFERENCIA != ""
+                                             && p.RC_COD_AREA == (pagingparametermodel.areaOperativa == null ? p.RC_COD_AREA : pagingparametermodel.areaOperativa)
+                                             && p.RC_COD_OPERACION == (pagingparametermodel.tipoReporte == null ? p.RC_COD_OPERACION : pagingparametermodel.tipoReporte)
+                                             && p.PA_FECHA_CARGA == (pagingparametermodel.fechaCargaIni == null ? p.PA_FECHA_CARGA : pagingparametermodel.fechaCargaIni)
+                                             && p.PA_FECHA_CARGA == (pagingparametermodel.fechaCargaFin == null ? p.PA_FECHA_CARGA : pagingparametermodel.fechaCargaFin)
+                                             && p.PA_CTA_CONTABLE == (pagingparametermodel.cuentaContable == null ? p.PA_CTA_CONTABLE : pagingparametermodel.cuentaContable)
+                                             && p.PA_REFERENCIA == (pagingparametermodel.referencia == null ? p.PA_REFERENCIA : pagingparametermodel.referencia)
+                                             && p.PA_FECHA_TRX >= (pagingparametermodel.fechaTransaccionIni == null ? p.PA_FECHA_TRX : pagingparametermodel.fechaTransaccionIni)
+                                             && p.PA_FECHA_TRX <= (pagingparametermodel.fechaTransaccionFin == null ? p.PA_FECHA_TRX : pagingparametermodel.fechaTransaccionFin)
+                                             
+                                             );
+                if (model.Count() > 0)
+                {
+                    int count = model.Count();
+                    int CurrentPage = pagingparametermodel.pageNumber;
+                    int PageSize = pagingparametermodel.pageSize;
+                    int TotalCount = count;
+                    int TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+                    var items = model.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
+                    var previousPage = CurrentPage > 1 ? "Yes" : "No";
+                    var nextPage = CurrentPage < TotalPages ? "Yes" : "No";
+                    var paginationMetadata = new
+                    {
+                        totalCount = TotalCount,
+                        pageSize = PageSize,
+                        currentPage = CurrentPage,
+                        totalPages = TotalPages,
+                        previousPage,
+                        nextPage,
+                        data = items
+
+                    };
+                    //HttpContext.Current.Response.Headers.Add("Paging-Headers", JsonConvert.SerializeObject(paginationMetadata));
+                    return Ok(paginationMetadata);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
         [Route("GetPartidasByUser")]
         public IHttpActionResult GetPartidasByUser(String id)
         {
