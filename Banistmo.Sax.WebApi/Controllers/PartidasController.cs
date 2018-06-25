05710df274be0efceb9470db8a32420db5b24623
larcia <linette.arcia@gmail.com>
2018-06-25 09:48:12 -0500

    Reporte Coontrol de Supervisor

--- Banistmo.Sax.WebApi/Controllers/PartidasController.cs
+++ Banistmo.Sax.WebApi/Controllers/PartidasController.cs
@@ -23,7 +23,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Banistmo.Sax.Repository.Model;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using Banistmo.Sax.Repository.Model;
 
 namespace Banistmo.Sax.WebApi.Controllers
 {
