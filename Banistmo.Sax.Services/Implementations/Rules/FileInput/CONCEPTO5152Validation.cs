582400fdea01a6bd37f681c0948be1198636674f
VictorMurillo <VictorMurillo@DESKTOP-OIGED8S>
2018-07-27 10:02:46 -0500

    Fix en la regal de concepto de costo  para las cuents 51 52 31, 32

--- Banistmo.Sax.Services/Implementations/Rules/FileInput/CONCEPTO5152Validation.cs
+++ Banistmo.Sax.Services/Implementations/Rules/FileInput/CONCEPTO5152Validation.cs
@@ -101,6 +101,10 @@ namespace Banistmo.Sax.Services.Implementations.Rules.FileInput
                 else if (Context.PA_COD_EMPRESA == this.EmpresaFinancomer && (Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("51") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("52") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("31") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("32"))) {
                     return false;
                 }
+                else if (Context.PA_COD_EMPRESA != this.EmpresaFinancomer && (Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("51") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("52") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("31") || Context.PA_CTA_CONTABLE.Trim().Substring(0, 2).Equals("32")))
+                {
+                    return false;
+                }
                 return true;
 
             }
