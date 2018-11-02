14afdb94516e4704e94f109e0cc7398a78a2719e
VictorMurillo <VictorMurillo@DESKTOP-OIGED8S>
2018-11-02 16:37:35 -0500

    Cambio de nombre de columna

--- Banistmo.Sax.WebApi/Controllers/CuentaContableController.cs
+++ Banistmo.Sax.WebApi/Controllers/CuentaContableController.cs
@@ -400,7 +400,7 @@ namespace Banistmo.Sax.WebApi.Controllers
             {
                 Empresa = NameEmpresa(c.CE_ID_EMPRESA),
                 CuentaContable = $"{c.CO_CUENTA_CONTABLE}{c.CO_COD_AUXILIAR}{c.CO_NUM_AUXILIAR.Trim()}",
-                NombreCuenta = c.CO_NOM_CUENTA.Trim(),
+                NombreCuenta = c.CO_NOM_AUXILIAR.Trim(),
                 Concilia = GetConcilia(c.CO_COD_CONCILIA),
                 Naturaleza = GetNaturaleza(c.CO_COD_NATURALEZA),
                 AreaOperativa = NameAreaOperativa(c.ca_id_area)
